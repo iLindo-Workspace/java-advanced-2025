b146b41e27bede8a4f5957e8c27b4c5aeee9c93b
iLindo-Workspace <Lindokuhle.Makanda@ilabqa.com>
2025-09-25 08:55:46 +0200

    Completed Exercise 8.2. close #7

---
+++ JavaAdvancedTraining/src/main/java/Chaper8/Exercises/DepartmentException.java
@@ -0,0 +1,22 @@
+package Chaper8.Exercises;
+
+/**
+ * @author Lindokuhle Makanda
+ * Thrown when the {@code Department} has invalid input for its field
+ */
+public class DepartmentException extends Exception{
+    /**
+     * Constructs an {@code DepartmentException} with the specified message
+     * @param message The detail message
+     */
+    public DepartmentException(String message, String dep, int depID, int numOfEmployees){
+
+        super("Failed to create Department: " + dep +
+                "\nDepartmentID : " + depID +
+                "\nNumber of Employees: " + numOfEmployees +
+                "\n" + message + "\n\n");
+    }
+
+    public DepartmentException(String message) {
+    }
+}
